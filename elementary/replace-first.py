ed247c06905df9ecde734fa751d1baafaf1e7b6a
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2021-02-08 17:02:11 -0300

    Adicionando exercicios da primeira ilha

---
+++ elementary/replace-first.py
@@ -0,0 +1,22 @@
+from typing import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    # your code here
+    if not items:
+        return items
+    aux = items[0]
+    items.remove(items[0])
+    items.append(aux)
+    return items
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
